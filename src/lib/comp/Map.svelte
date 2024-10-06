c28765145aad2759d2a198480059e03d072c2602
Alan Monroy Bernedo <alan.monroy@ucsp.edu.pe>
2024-10-06 12:50:14 -0500

    mambo

--- src/lib/comp/Map.svelte
+++ src/lib/comp/Map.svelte
@@ -13,6 +13,34 @@
 			<slot/>
 		</article>
 	</div>
+	<dialog open>
+		<div class="fc between">
+			<h1>Capas</h1>
+			<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="var(--white)" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-x"><path d="M18 6 6 18"/><path d="m6 6 12 12"/></svg>
+		</div>
+		<div class="fcol g16">
+			<label for="disasters" class="fc g12">
+				<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="var(--green)" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-trees"><path d="M10 10v.2A3 3 0 0 1 8.9 16H5a3 3 0 0 1-1-5.8V10a3 3 0 0 1 6 0Z"/><path d="M7 16v6"/><path d="M13 19v3"/><path d="M12 19h8.3a1 1 0 0 0 .7-1.7L18 14h.3a1 1 0 0 0 .7-1.7L16 9h.2a1 1 0 0 0 .8-1.7L13 3l-1.4 1.5"/></svg>
+				<p class="grow">Desastres Naturales</p>
+				<input class="toggle" type="checkbox" name="disasters" id="disasters">
+			</label>
+			<label for="disasters" class="fc g12">
+				<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="var(--red)" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-shield-alert"><path d="M20 13c0 5-3.5 7.5-7.66 8.95a1 1 0 0 1-.67-.01C7.5 20.5 4 18 4 13V6a1 1 0 0 1 1-1c2 0 4.5-1.2 6.24-2.72a1.17 1.17 0 0 1 1.52 0C14.51 3.81 17 5 19 5a1 1 0 0 1 1 1z"/><path d="M12 8v4"/><path d="M12 16h.01"/></svg>
+				<p class="grow">Seguridad</p>
+				<input class="toggle" type="checkbox" name="disasters" id="disasters">
+			</label>
+			<label for="disasters" class="fc g12">
+				<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="var(--sky)" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-users-round"><path d="M18 21a8 8 0 0 0-16 0"/><circle cx="10" cy="8" r="5"/><path d="M22 20c0-3.37-2-6.5-4-8a5 5 0 0 0-.45-8.3"/></svg>
+				<p class="grow">Población</p>
+				<input class="toggle" type="checkbox" name="disasters" id="disasters">
+			</label>
+			<label for="disasters" class="fc g12">
+				<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="var(--green)" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-hand-coins"><path d="M11 15h2a2 2 0 1 0 0-4h-3c-.6 0-1.1.2-1.4.6L3 17"/><path d="m7 21 1.6-1.4c.3-.4.8-.6 1.4-.6h4c1.1 0 2.1-.4 2.8-1.2l4.6-4.4a2 2 0 0 0-2.75-2.91l-4.2 3.9"/><path d="m2 16 6 6"/><circle cx="16" cy="9" r="2.9"/><circle cx="6" cy="5" r="3"/></svg>
+				<p class="grow">Nivel Socio-económico</p>
+				<input class="toggle" type="checkbox" name="disasters" id="disasters">
+			</label>
+		</div>
+	</dialog>
 </main>
 
 <style>
@@ -26,7 +54,7 @@
 		right: 16px;
 		bottom: 16px;
 	}
-	button, article {
+	button, article, dialog {
 		background: var(--bg);
 	}
 	button {
@@ -43,5 +71,17 @@
 		background: #1F1F20;
 		width: 100vw;
 		height: 100vh;
+	}
+	dialog {
+		width: 80%;
+		max-width: 360px;
+		border: none;
+		color: var(--text);
+	}
+	h1 {
+		font-size: 32px;
+	}
+	label p {
+		user-select: none;
 	}
 </style>
--- src/lib/comp/Search.svelte
+++ src/lib/comp/Search.svelte
@@ -1,6 +1,6 @@
 <form class="fc g8 px16 py12">
 	<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="var(--white)" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M2 10v3"/><path stroke="var(--orange)" d="M6 6v11"/><path d="M10 3v18"/><path d="M14 8v7"/><path d="M18 5v13"/><path d="M22 10v3"/></svg>
-	<input type="text" name="query" placeholder="Buscar zonas...">
+	<input class="grow" type="text" name="query" placeholder="Buscar zonas...">
 	<button type="submit">
 		<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-search"><circle cx="11" cy="11" r="8"/><path d="m21 21-4.3-4.3"/></svg>
 	</button>
@@ -18,7 +18,6 @@
 		top: 16px;
 	}
 	input {
-		flex: 1;
 		color: var(--white);
 	}
 	input:focus {
