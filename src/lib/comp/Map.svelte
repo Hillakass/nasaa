34f7d5ef5e9ae30f1341475e84bc419371f80e74
Alan Monroy Bernedo <alan.monroy@ucsp.edu.pe>
2024-10-06 22:04:13 -0500

    about

--- src/lib/comp/Map.svelte
+++ src/lib/comp/Map.svelte
@@ -261,6 +261,9 @@
 		}}>
 			<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="var(--white)" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="m16.02 12 5.48 3.13a1 1 0 0 1 0 1.74L13 21.74a2 2 0 0 1-2 0l-8.5-4.87a1 1 0 0 1 0-1.74L7.98 12"/><path d="M13 13.74a2 2 0 0 1-2 0L2.5 8.87a1 1 0 0 1 0-1.74L11 2.26a2 2 0 0 1 2 0l8.5 4.87a1 1 0 0 1 0 1.74Z"/></svg>
 		</button>
+		<a href="/about" class="about p12">
+			<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="var(--white)" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-circle-help"><circle cx="12" cy="12" r="10"/><path d="M9.09 9a3 3 0 0 1 5.83 1c0 2-3 3-3 3"/><path d="M12 17h.01"/></svg>
+		</a>
 		<article class="fcol px24 py32 g16">
 			<slot/>
 		</article>
@@ -309,13 +312,22 @@
 		right: 16px;
 		bottom: 16px;
 	}
-	button, article, dialog {
+	button, .about, article, dialog {
 		background: var(--bg);
 	}
-	button {
+	button, .about {
 		border-radius: 12px;
 	}
-	button:hover {
+	.layers, .about {
+		position: absolute;
+	}
+	.layers {
+		top: -60px;
+	}
+	.about {
+		top: -64px;
+	}
+	button:hover, .about:hover {
 		background: #232422;
 	}
 	article {
---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,32 @@
+
+<header class="p16">
+	<a href="/" class="fc g8">
+		<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="var(--white)" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M2 10v3"/><path stroke="var(--orange)" d="M6 6v11"/><path d="M10 3v18"/><path d="M14 8v7"/><path d="M18 5v13"/><path d="M22 10v3"/></svg>
+		<h1>Geo<span>Alert</span></h1>
+	</a>
+</header>
+<main class="fcol p16 g16">
+	<h1>Sobre el proyecto</h1>
+	<h2>Nosotros</h2>
+	<p>Somos un grupo multidisciplinar interesados por la investigación y la ciencia, participantes del Hackaton 2024 NASA Space Apps Challenge Community Mapping.</p>
+	<p>Geoalert es una aplicación web progresiva (PWA) que ayuda a sus usuarios a mantenerse alerta e informados sobre varios desastres naturales y sus consecuencias en el medio ambiente, contando con información actualizada en un lapso de 4 días, mostramos al usuario un mapa interactivo con mucha información relevante.	</p>
+	<h2>Otro sbtítulo</h2>
+	<p>Blah blah blah</p>
+	<p>Blah blah blah</p>
+</main>
+
+<style>
+	h1 {
+		color: white;
+	}
+	span {
+		color: var(--orange);
+	}
+	header a {
+		text-decoration: none;
+		color: white;
+	}
+	header h1 {
+		font-size: 1.5em;
+	}
+</style>
